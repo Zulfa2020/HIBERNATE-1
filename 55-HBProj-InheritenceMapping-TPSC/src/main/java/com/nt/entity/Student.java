834c32f6a495598a0904123b73dc47dfc1511f9b
tl <HP@DESKTOP-PDMUKAP>
2020-10-27 10:01:57 +0530

    inheritance mapping

---
+++ 55-HBProj-InheritenceMapping-TPSC/src/main/java/com/nt/entity/Student.java
@@ -0,0 +1,40 @@
+package com.nt.entity;
+
+
+public class Student extends Person {
+    private String college;
+    private String branch;
+    private  Float avg;
+	
+    public String getCollege() {
+		return college;
+	}
+
+	public void setCollege(String college) {
+		this.college = college;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public Float getAvg() {
+		return avg;
+	}
+
+	public void setAvg(Float avg) {
+		this.avg = avg;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() +"Student [college=" + college + ", branch=" + branch + ", avg=" + avg + ", toString()="
+				+  "]";
+	}
+    
+    
+}
